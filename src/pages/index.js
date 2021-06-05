5dda669fb76ca95e6d42138bfcf544628aa6a476
Jason Wong <jasonw@Jasons-MacBook-Pro-2.local>
2021-06-05 14:21:20 -0700

    main page with styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,40 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Seo title="jwaosnogn" />
+    <header className="header">
+      <p className="header-letter header-letter--1">j</p>
+      <p className="header-letter header-letter--6">w</p>
+      <p className="header-letter header-letter--2">a</p>
+      <p className="header-letter header-letter--7">o</p>
+      <p className="header-letter header-letter--3">s</p>
+      <p className="header-letter header-letter--8">n</p>
+      <p className="header-letter header-letter--4">o</p>
+      <p className="header-letter header-letter--9">g</p>
+      <p className="header-letter header-letter--5">n</p>
+    </header>
+    <p className="title">
+      Full Stack Software Engineer at{" "}
+      <a href="https://www.gocurrency.com/" target="_blank">
+        Currency Capital
+      </a>
     </p>
+    <nav className="navigation">
+      <ul className="navigation-list">
+        <li className="navigation-list-item navigation-list-item--1">
+          <a href="https://github.com/jwaosnogn" target="_blank">Github</a>
+        </li>
+        <li className="navigation-list-item navigation-list-item--2">
+          <a href="https://www.linkedin.com/in/jwong1995" target="_blank">LinkedIn</a>
+        </li>
+        <li className="navigation-list-item navigation-list-item--3">
+          <a href="https://jwaosnogn.github.io/Jason%20Wong%20Resume.pdf">Resume</a>
+        </li>
+      </ul>
+    </nav>
   </Layout>
 )
 
